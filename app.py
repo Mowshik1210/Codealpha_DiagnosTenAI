d5d173416c8d5e223bc295587938d1d74ac630b5
Mowshik1210 <dharshanmowshik@gmail.com>
2026-06-23 13:55:13 +0530

    port setting

--- app.py
+++ app.py
@@ -580,5 +580,5 @@ def download_report():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
